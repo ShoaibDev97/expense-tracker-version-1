73a9d5efa9b10a825e86d4fcd0f1954966017814
Muhammad Shoaib <shoaib@manhattan.leoburnett.com.pk>
2022-01-11 15:48:04 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ExpenseItem />
+      </div>
+    )
+  }
+}
+
+export default App
